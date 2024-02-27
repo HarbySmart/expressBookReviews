9105980cfa6d96ee03e77ff606ca29c75c661646
HarbySmart <aderemiabeeb@gmail.com>
2024-02-27 02:46:29 -0500

    updated router

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,27 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const books = req.params.books;
+  const author = object.keys(books);
+
+  for(const keys in books) {
+    if(books[keys].author === author){
+        return
+    }
+  }
 });
 
 // Get all books based on title
@@ -37,7 +45,8 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
